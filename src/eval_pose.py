59d92ce9281430500470e69b665a032807d61e00
dexter2406 <46349448+dexter2406@users.noreply.github.com>
2021-04-23 11:28:24 +0200

    bug fix, support external poses eval

--- src/eval_pose.py
+++ src/eval_pose.py
@@ -3,84 +3,100 @@ import numpy as np
 from datasets.dataset_kitti import KITTIOdom
 from datasets.data_loader_kitti import DataLoader
 from utils import readlines
-from models.depth_decoder_creater import DepthDecoder_full
 from models.encoder_creater import ResNet18_new
+from models.posenet_decoder_creator import PoseDecoder
 from src.eval_helper import dump_xyz, compute_ate
 from src.DataPprocessor import DataProcessor
 from src.trainer_helper import build_models, transformation_from_parameters
 from options import get_options
 import tensorflow as tf
+from absl import app
+import warnings
 
-root_dir = os.path.dirname(__file__)
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
 
 def evalualte_pose(opt):
     assert os.path.isdir(opt.weights_dir), 'folder %s doesn\'t exists' % opt.weights_dir
 
-    assert opt.eval_split == "odom_9" or opt.eval_split == "odom_10", \
-        "eval_split should be either odom_9 or odom_10"
+    if opt.eval_split not in ['odom_9', 'odom_10']:
+        warnings.warn("eval_split should be either odom_9 or odom_10")
+        alter = input('use odom_9 instead? y/n\n')
+        if alter == 'y':
+            opt.eval_split = 'odom_9'
 
     sequence_id = int(opt.eval_split.split("_")[1])
-    filenames = readlines(
-        os.path.join(root_dir, "splits", "odom",
-                     "test_files_{:02d}.txt".format(sequence_id)))
+    split_folder = opt.eval_split.split("_")[0]
+    opt.data_path = r'E:\Datasets\KITTI\KITTI_odom\kitti_odom'
+    split_folder = os.path.join('splits', split_folder)
+    split_name = 'test_files_{:02d}.txt'.format(int(opt.eval_split[-1]))
 
-    # ----------
-    # Get dataset
-    # ----------
-    opt.frame_idx = [0, 1]
-    split_folder = os.path.join('splits', opt.eval_split)
-    split_name = 'test_files{:02d}.txt'.format(int(opt.eval_split[-1]))
-    dataset = KITTIOdom(split_folder, split_name, data_path=opt.data_path)
-    data_loader = DataLoader(dataset, num_epoch=1,
-                             batch_size=2, frame_idx=opt.frame_idx)
-    eval_iter = data_loader.build_eval_dataset()
-    batch_processor = DataProcessor(frame_idx=opt.frame_idx, intrinsics=dataset.K)
+    print(opt.use_ext_res)
+    if opt.use_ext_res is not None:
+        pred_poses = np.load(opt.use_ext_res)
 
-    # ----------
-    # Load Models
-    # ----------
-    models = {
-        'pose_enc': ResNet18_new([2, 2, 2, 2]),
-        'pose_dec': DepthDecoder_full()
-    }
-    build_models(models)
+    else:
+        # filenames = readlines(
+        #     os.path.join(root_dir, "splits", "odom",
+        #                  "test_files_{:02d}.txt".format(sequence_id)))
+        # ----------
+        # Get dataset
+        # ----------
+        print('-> Preparing dataset...')
 
-    for m_name in models.keys():
-        m_path = os.path.join(opt.weights_dir, m_name + '.h5')
-        models[m_name].load_weights(m_path)
+        dataset = KITTIOdom(split_folder, split_name, data_path=opt.data_path)
 
-    # ----------
-    # Generate poses
-    # ----------
-    print("-> Computing pose predictions")
-    pred_poses = []
+        opt.frame_idx = [0, 1]
+        num_scales = 1
+        batch_size = 16
+        data_loader = DataLoader(dataset, num_epoch=1,
+                                 batch_size=batch_size, frame_idx=opt.frame_idx)
+        eval_iter = data_loader.build_eval_dataset()
+        batch_processor = DataProcessor(frame_idx=opt.frame_idx, num_scales=num_scales,
+                                        intrinsics=dataset.K)
 
-    for i in range(len(filenames)):
-        batch = eval_iter.get_next()
-        input_imgs, input_Ks = batch_processor.prepare_batch(batch)
-        pose_inps = [input_imgs[('color_aug', i, 0)] for i in opt.frame_idx]
+        # ----------
+        # Load Models
+        # ----------
+        models = {
+            'pose_enc': ResNet18_new([2, 2, 2, 2]),
+            'pose_dec': PoseDecoder(num_ch_enc=[64, 64, 128, 256, 512])
+        }
+        build_models(models)
+        for m_name in models.keys():
+            m_path = os.path.join(opt.weights_dir, m_name + '.h5')
+            models[m_name].load_weights(m_path)
 
-        features = models['pose_enc'](tf.concat(pose_inps), axis=3)
-        pose_outs = models['pose_dec'](features)
-        axisangle, translation = pose_outs['angles'], pose_outs['translations']
+        # ----------
+        # Generate poses
+        # ----------
+        print("-> Generating pose predictions...")
+        pred_poses = []
+        for batch in eval_iter:
+            input_imgs, input_Ks = batch_processor.prepare_batch(batch)
+            pose_inps = [input_imgs[('color_aug', i, 0)] for i in opt.frame_idx]
 
-        pred = transformation_from_parameters(axisangle[:, 0], translation[:, 0])
-        pred_poses.append(pred)
+            features = models['pose_enc'](tf.concat(pose_inps, axis=3))
+            pose_outs = models['pose_dec'](features)
+            axisangle, translation = pose_outs['angles'], pose_outs['translations']
+            pred = transformation_from_parameters(axisangle[:, 0], translation[:, 0])
+            pred_poses.append(pred)
 
-    pred_poses = np.concatenate(pred_poses)
+        pred_poses = np.concatenate(pred_poses)
 
     # ----------
     # Load file of ground-truth poses
     # ----------
-    gt_poses_dir = os.path.join(root_dir, "outputs", "poses")
+    print("-> Load poses of ground-truth files...")
+    gt_poses_dir = os.path.join(split_folder, "outputs", "poses")
     if not os.path.isdir(gt_poses_dir):
         os.makedirs(gt_poses_dir)
     gt_poses_path = os.path.join(gt_poses_dir, "{:02d}.txt".format(sequence_id))
 
     # ----------
-    # Coordinates transormations
+    # Coordinates transformations
     # ----------
+    print('\ttransforming gt coordinates from global to local poses')
     #  - global poses
     gt_global_poses = np.loadtxt(gt_poses_path).reshape((-1, 3, 4))
     gt_global_poses = np.concatenate(
@@ -96,6 +112,7 @@ def evalualte_pose(opt):
     # ----------
     # Compute trajectory errors
     # ----------
+    print("-> Computing trajectory errors...")
     ates = []
     num_frames = gt_xyzs.shape[0]
     track_length = 5
@@ -112,6 +129,10 @@ def evalualte_pose(opt):
     print("-> Predictions saved to", save_path)
 
 
-if __name__ == '__main__':
+def main_test(_):
     options = get_options()
     evalualte_pose(options)
+
+
+if __name__ == "__main__":
+    app.run(main_test)
