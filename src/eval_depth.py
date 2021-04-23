ec653c1b14aeb21559a09bb74802197e726e3541
dexter2406 <46349448+dexter2406@users.noreply.github.com>
2021-04-23 11:27:23 +0200

    bug fix

--- src/eval_depth.py
+++ src/eval_depth.py
@@ -11,10 +11,12 @@ from src.DataPprocessor import DataProcessor
 from src.eval_helper import compute_errors
 from options import get_options
 import cv2 as cv
+from absl import app
 
 
 root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 splits_dir = os.path.join(root_dir, "splits")
+depth_model_names = ['depth_enc', 'depth_dec']
 
 
 def evaluate_depth(opt):
@@ -23,50 +25,59 @@ def evaluate_depth(opt):
     MIN_DEPTH = 1e-3
     MAX_DEPTH = 80
 
-    if not opt.use_ext_disp:
+    if opt.use_ext_res is not None:
         # ----------
         # Prepare models
         # ----------
-        assert os.path.isdir(opt.weights_dir), 'folder %s doesn\'t exists' % opt.weights_dir
+        assert os.path.isdir(opt.weights_dir), 'folder: %s doesn\'t exists' % opt.weights_dir
         print('-> Loading weights from {}'.format(opt.weights_dir))
-        filenames = readlines(os.path.join(splits_dir, opt.eval_split, "test_files.txt"))
-        models = {}
+        # filenames = readlines(os.path.join(splits_dir, opt.eval_split, "test_files.txt"))
 
-        models['depth_enc'] = ResNet18_new([2, 2, 2, 2])
-        models['depth_dec'] = DepthDecoder_full()
+        models = {'depth_enc': ResNet18_new([2, 2, 2, 2]),
+                  'depth_dec': DepthDecoder_full()}
         build_models(models)
-
-        for m_name in ['depth_enc', 'depth_dec']:
+        for m_name in depth_model_names:
             m_path = os.path.join(opt.weights_dir, m_name + '.h5')
             models[m_name].load_weights(m_path)
 
         # ----------
         # Get dataset
         # ----------
+        print('-> Preparing dataset: KITTI_Raw...')
         split_folder = os.path.join('splits', opt.eval_split)
         split_name = 'test_files.txt'.format(opt.eval_split)
+        path_tmp = os.path.join(split_folder, split_name)
+        assert os.path.isfile(path_tmp), '%s is not valid path to split files' % path_tmp
+
+        opt.frame_idx = [0]
+        num_scales = 1
+        batch_size = 16
         dataset = KITTIRaw(split_folder, split_name, data_path=opt.data_path)
         data_loader = DataLoader(dataset, num_epoch=1,
-                                 batch_size=2, frame_idx=opt.frame_idx)
+                                 batch_size=batch_size, frame_idx=opt.frame_idx)
         eval_iter = data_loader.build_eval_dataset()
-        batch_processor = DataProcessor(frame_idx=opt.frame_idx, intrinsics=dataset.K)
+        batch_processor = DataProcessor(frame_idx=opt.frame_idx, num_scales=num_scales,
+                                        intrinsics=dataset.K)
 
         # ----------
         # Generate predicted disparity map
         # ----------
+        print('-> Generate predicted disparity map...')
         pred_disps = []
-        for i in range(len(filenames)):
-            batch = eval_iter.get_next()
-            input_imgs, input_Ks = batch_processor.prepare_batch(batch)
+        output = {}
+        for batch in eval_iter:
+            # batch = eval_iter.get_next()
+            input_imgs, input_Ks = batch_processor.prepare_batch_val(batch)
             input_color = input_imgs[('color', 0, 0)]
-
-            output = models['depth_dec'](models['depth_enc'](input_color))
-            pred_disp, _ = disp_to_depth(output[("disp", 0)], opt.min_depth, opt.max_depth)
-            pred_disp = pred_disp[..., 0].numpy()
-
+            disp_raw = models['depth_dec'](models['depth_enc'](input_color))
+            output[('disp', 0)] = disp_raw['output_0']
+            pred_disp, _ = disp_to_depth(output[('disp', 0)], opt.min_depth, opt.max_depth)
+            pred_disp = pred_disp[..., 0].numpy()   # squeeze the last dim
             pred_disps.append(pred_disp)
 
-    # use imported disp to eval
+        pred_disps = np.concatenate(pred_disps)
+
+    # todo: use imported disp to eval
     else:
         print("-> Loading predictions from {}".format(opt.ext_disp_to_eval))
         pred_disps = np.load(opt.ext_disp_to_eval)
@@ -83,6 +94,7 @@ def evaluate_depth(opt):
             os.makedirs(output_dir)
         output_path = os.path.join(output_dir, 'disps_{}_split.npy'.format(opt.eval_split))
         print('-> Saving predicted disparities to ', output_path)
+        np.save(output_path, pred_disps)
 
     # Just need generate predictions, but not to evaluate them
     if opt.no_eval:
@@ -92,6 +104,7 @@ def evaluate_depth(opt):
     # ----------
     # Get ground truth and start evaluation
     # ----------
+    print("-> Loading depth ground truth...")
     gt_path = os.path.join(splits_dir, opt.eval_split, 'gt_depths.npz')
     gt_depths = np.load(gt_path, fix_imports=True, encoding='latin1', allow_pickle=True)["data"]
 
@@ -117,7 +130,7 @@ def evaluate_depth(opt):
         else:
             mask = gt_depth > 0
 
-        pred_depth = pred_depth[mask]
+        pred_depth = pred_depth[mask]   # tf.boolean_mask
         gt_depth = gt_depth[mask]
 
         pred_depth *= opt.pred_depth_scale_factor
@@ -142,6 +155,10 @@ def evaluate_depth(opt):
     print("\n-> Done!")
 
 
-if __name__ == "__main__":
+def main_test(_):
     options = get_options()
     evaluate_depth(options)
+
+
+if __name__ == "__main__":
+    app.run(main_test)
