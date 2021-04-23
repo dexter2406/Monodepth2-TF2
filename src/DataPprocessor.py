701c20373b41b5c77d54e295a3a3961a0655a85b
dexter2406 <46349448+dexter2406@users.noreply.github.com>
2021-04-23 11:26:43 +0200

    choice of using @tf.function for val/train data

--- src/DataPprocessor.py
+++ src/DataPprocessor.py
@@ -6,18 +6,17 @@ import numpy as np
 
 
 class DataProcessor(object):
-    def __init__(self, frame_idx, intrinsics=None, is_train=False):
+    def __init__(self, frame_idx, intrinsics=None, is_train=False, num_scales=4):
         self.is_train = is_train
-        self.num_scales = 4
+        self.num_scales = num_scales     # for evaluation, 1
         self.height = 192
         self.width = 640
         self.frame_idx = frame_idx
         self.batch_size = -1
-        # For KITTI. Must *normalize* when using on different scale
         self.K = intrinsics
 
     @tf.function
-    def prepare_batch(self, tgt_batch, src_batch):
+    def prepare_batch(self, batch):
         """Apply augmentation
         tgt_batch: ndarray
             - [<Batch>, 192, 640, 3]
@@ -34,24 +33,39 @@ class DataProcessor(object):
         src_image_stack_aug = input_imgs['color_aug', 1:, :]
         tgt_image_pyramid = input_imgs['color, 0, :]
         """
-        do_color_aug = self.is_train and np.random.random() > 0.5
-        do_flip = False
-        # print("\t check input batch: ", tgt_batch.shape," and ", src_batch.shape)
+        # do_color_aug = self.is_train and np.random.random() > 0.5
+        # do_flip = False
+        input_imgs, input_Ks = {}, {}
+        input_imgs, input_Ks = self.process_batch_main(batch, input_imgs, input_Ks)
+        return input_imgs, input_Ks
+
+    def prepare_batch_val(self, batch):
+        """duplicate of prepare_batch(), no @tf.function decorator
+        For validation OR evaluation
+        """
+        input_imgs, input_Ks = {}, {}
+        input_imgs, input_Ks = self.process_batch_main(batch, input_imgs, input_Ks)
+        return input_imgs, input_Ks
+
+    def process_batch_main(self, batch, input_imgs, input_Ks):
+        if type(batch) == tuple:
+            batch_imgs, batch_depths = batch
+            input_imgs['depth_gt'] = tf.expand_dims(batch_depths, 3)
+        else:
+            batch_imgs = batch
+        tgt_batch, src_batch = batch_imgs[..., :3], batch_imgs[..., 3:]
         self.batch_size = tgt_batch.shape[0]
 
-        input_imgs, input_Ks = {}, {}
+        # in training / eval_depth / eval_pose
+        # frame_idx == [0,-1,1] / [0] / [0,1] respectively
         input_imgs[('color', self.frame_idx[0], -1)] = tgt_batch
-        input_imgs[('color', self.frame_idx[1], -1)] = src_batch[..., :3]
-        input_imgs[('color', self.frame_idx[2], -1)] = src_batch[..., 3:]
+        if len(self.frame_idx) > 1:
+            for i, f_i in enumerate(self.frame_idx[1:]):
+                input_imgs[('color', f_i, -1)] = src_batch[..., i*3: (i+1)*3]
 
         input_imgs, input_Ks = self.preprocess(input_imgs, input_Ks)
         self.delete_raw_images(input_imgs)
-
         return input_imgs, input_Ks
-
-    def delete_raw_images(self, input_imgs, scale_to_del=-1):
-        for idx in self.frame_idx:
-            del input_imgs[('color', self.frame_idx[idx], scale_to_del)]
 
     def preprocess(self, input_imgs, input_Ks):
         """Make pyramids and augmentations
@@ -59,19 +73,21 @@ class DataProcessor(object):
         - augment: correspond to the pyramid source
         """
         # print("\t preprocessing batch...")
-        intrinsics_mscale = []
         for k in list(input_imgs):
+            if type(k) is not tuple:
+                continue    # skip input_imgs['depth_dt']
             img_type, f_i, scale = k    # key components
-            assert img_type == 'color'
-            for scale in range(self.num_scales):
-                src_image = input_imgs[(img_type, f_i, scale - 1)]
-                resized_image = tf.image.resize(src_image,
-                                                (self.height//(2**scale), self.width//(2**scale)),
-                                                antialias=True)
-                input_imgs[(img_type, f_i, scale)] = resized_image
-                input_imgs[(img_type + '_aug', f_i, scale)] = self.color_aug(resized_image)
+            if img_type == 'color':
+                for scale in range(self.num_scales):
+                    src_image = input_imgs[(img_type, f_i, scale - 1)]
+                    resized_image = tf.image.resize(src_image,
+                                                    (self.height//(2**scale), self.width//(2**scale)),
+                                                    antialias=True)
+                    input_imgs[(img_type, f_i, scale)] = resized_image
+                    input_imgs[(img_type + '_aug', f_i, scale)] = self.color_aug(resized_image)
 
         for scale in range(self.num_scales):
+            # For KITTI. Must *normalize* when using on different scale
             K = self.K.copy()
             K[0, :] *= self.width // (2 ** scale)
             K[1, :] *= self.height // (2 ** scale)
@@ -87,16 +103,9 @@ class DataProcessor(object):
 
         return input_imgs, input_Ks
 
-    def make_intrinsics_matrix(self, fx, fy, cx, cy):
-        # Assumes batch input
-        batch_size = fx.shape[0]
-        zeros = tf.zeros_like(fx)
-        r1 = tf.stack([fx, zeros, cx], axis=1)
-        r2 = tf.stack([zeros, fy, cy], axis=1)
-        r3 = tf.constant([0., 0., 1.], shape=[1, 3])
-        r3 = tf.tile(r3, [batch_size, 1])
-        intrinsics = tf.stack([r1, r2, r3], axis=1)
-        return intrinsics
+    def delete_raw_images(self, input_imgs, scale_to_del=-1):
+        for idx in self.frame_idx:
+            del input_imgs[('color', self.frame_idx[idx], scale_to_del)]
 
     def color_aug(self, image):
         # todo: implement color augmentations
