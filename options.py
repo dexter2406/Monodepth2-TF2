3b9ad21b7c12b7e56ffd2563c1ccf2310a0b3771
dexter2406 <46349448+dexter2406@users.noreply.github.com>
2021-04-27 23:08:15 +0200

    Update options.py

--- options.py
+++ options.py
@@ -5,26 +5,29 @@ import datetime
 
 rootdir = os.path.dirname(__file__)
 current_time = datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
-all_models = ['depth_enc', 'depth_dec', 'pose_enc', 'pose_enc']
+all_models = ['depth_enc', 'depth_dec', 'pose_enc', 'pose_dec']
 FLAGS = flags.FLAGS
 
 # Pre-settings
-flags.DEFINE_string('weights_dir', r'.\logs\weights\epoch_4\weights_0',  'the folder that stores weights files.')
+flags.DEFINE_string('weights_dir', '',  'the folder that stores weights files.')
 flags.DEFINE_list('models_to_load', all_models,
                   'load weights for specified models, by default all of them')
 flags.DEFINE_string('model_name', current_time, 'specify a dirname to collect weights, if not, current time is used')
 flags.DEFINE_bool('train_depth', True, 'whether to train depth decoder-encoder')
 flags.DEFINE_bool('train_pose', True, 'whether to train pose decoder-encoder')
-flags.DEFINE_bool('from_scratch', False, 'whether trained from scratch, coorperate with load_weights_folder')
 flags.DEFINE_string('save_model_path', '', 'path where weights are saved')
 flags.DEFINE_string('data_path', r'F:\Dataset\kitti_raw', 'path that stores corresponding dataset')
 flags.DEFINE_string('dataset', 'kitti_raw', 'specify a dataset, choices from [\'kitti_raw\', \'kitti_odom\']')
+flags.DEFINE_string('padding_mode', 'border', 'padding mode for bilinear sampler')
+flags.DEFINE_bool('mask_border', False, 'mask out the region padded by bilinear sampler '
+                                        'when computing losses (only for zero-padding)')
 
 # Training
+flags.DEFINE_bool('from_scratch', False, 'whether trained from scratch, coorperate with load_weights_folder')
 flags.DEFINE_string('run_mode', 'train', 'choose from [\'train\', \'eval_depth\', \'eval_pose\']')
 flags.DEFINE_string('split', 'eigen_zhou', 'training split, choose from: '
                                            '["eigen_zhou", "eigen_full", "odom", "benchmark"]')
-flags.DEFINE_bool('recording', False, 'whether to write results by tf.summary')
+flags.DEFINE_bool('recording', True, 'whether to write results by tf.summary')
 flags.DEFINE_string('record_summary_path', 'logs/gradient_tape/', 'root path to write summary')
 flags.DEFINE_integer('record_freq', 250, 'frequency to record')
 flags.DEFINE_integer('num_epochs', 10, 'total number of training epochs')
@@ -32,6 +35,7 @@ flags.DEFINE_integer('batch_size', 6, 'batch size')
 flags.DEFINE_bool('debug_mode', False, 'inspect intermediate results')
 flags.DEFINE_float('learning_rate', 1e-4, 'initial learning rate')
 flags.DEFINE_integer('lr_step_size', 15, 'step size to adapt learning rate (piecewise)')
+flags.DEFINE_integer('val_num_per_epoch', 10, 'validate how many times per epoch')
 
 # Model-related
 flags.DEFINE_integer('num_scales', 4, 'number of scales')
@@ -56,7 +60,6 @@ flags.DEFINE_string('eval_split', 'eigen', 'evaluation split, choose from: '
                                            '["eigen", "eigen_benchmark", "benchmark", "odom_9", "odom_10"]')
 flags.DEFINE_string('use_ext_res', None, 'use imported disparity predictions for evaluation, '
                                          'instead of generating them now')
-
 flags.mark_flag_as_required('run_mode')
 
 
