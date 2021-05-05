d8923bfe29c7bf33277e5a04dd4c7169cb6a9853
dexter2406 <46349448+dexter2406@users.noreply.github.com>
2021-05-05 19:48:44 +0200

    separate res_trans net

--- models/posenet_decoder_creator.py
+++ models/posenet_decoder_creator.py
@@ -3,6 +3,7 @@ from tensorflow.keras.layers import Conv2D
 import numpy as np
 import time
 
+
 class PoseDecoder(tf.keras.Model):
     def __init__(self, num_ch_enc=(64, 64, 128, 256, 512),
                  num_input_features=1, num_frames_to_predict_for=2, stride=1):
@@ -48,47 +49,25 @@ class PoseDecoder(tf.keras.Model):
 
 class PoseDecoder_exp(tf.keras.Model):
     """num_frames_to_predict=2, i.e. only frame 1->2, no 2->1"""
-    def __init__(self, pose_num, has_depth, num_ch_enc=(64, 64, 128, 256, 512),
-                 stride=1, has_residual_trans=False, do_automasking=False):
+    def __init__(self, has_depth, pose_num=1, num_ch_enc=(64, 64, 128, 256, 512),
+                 stride=1):
         super(PoseDecoder_exp, self).__init__()
         self.num_ch_enc = num_ch_enc
-        input_ch = 8 if has_depth else 6
+        self.out_ch = 8 if has_depth else 6
         # self.num_ch_dec = [input_ch, 16, 32, 64, 128, 256]
         self.num_ch_dec = [16, 32, 64, 128, 256]
         self.pose_scale = tf.cast(0.01, dtype=tf.float32)
         self.pose_num = pose_num
-        self.include_res_trans = has_residual_trans
-        self.do_automasking = do_automasking
         self.relu = tf.keras.activations.relu
 
         self.convs_squeeze = Conv2D(filters=256, kernel_size=1, name='Conv_squeeze')
 
         pose_0_nopad = Conv2D(256, kernel_size=3, strides=stride, padding="valid", name='Conv_pose_0')
         pose_1_nopad = Conv2D(256, kernel_size=3, strides=stride, padding='valid', name='Conv_pose_1')
-        filter_num_last = 6 * self.pose_num
+        filter_num_last = self.out_ch * self.pose_num
         pose_2_nopad = Conv2D(filter_num_last, kernel_size=1, strides=1, name='Conv_pose_2')
 
         self.convs_pose = [pose_0_nopad, pose_1_nopad, pose_2_nopad]
-
-        # ----------------------------
-        # Experiments: Residual Translation
-        # ----------------------------
-
-        if self.include_res_trans:
-            conv_prop_1 = {'kernel_size': 1, 'strides': 1, 'use_bias': False, 'padding': 'valid'}
-            self.conv_res_motion = Conv2D(input_ch, **conv_prop_1, name='unrefined_res_trans')
-            self.conv_refine_motion = []
-            for i, ch in enumerate(self.num_ch_dec):
-                idx = len(self.num_ch_dec) - i - 1     # 5->0
-                pref = 'Refine%d' % idx
-                conv_prop_2 = {'filters': ch, 'kernel_size': 3, 'strides': 1,
-                               'activation': 'relu', 'padding': 'valid'}
-                tmp = []
-                for j in range(3):
-                    tmp.append(Conv2D(**conv_prop_2, name=pref + 'Conv%d' % (j + 1)))
-                tmp.append(Conv2D(input_ch, [1, 1], strides=1,
-                                  activation=None, use_bias=False, name=pref + 'Conv4'))
-                self.conv_refine_motion.append(tmp)
 
     def call(self, input_features, training=None, mask=None):
         """ pass encoder-features pairwise
@@ -100,33 +79,71 @@ class PoseDecoder_exp(tf.keras.Model):
         backgrd_motion *= self.pose_scale
         angles = backgrd_motion[...,:, :, :3]
         trans = backgrd_motion[..., :, :, 3:]
-        assert angles.shape[1] == trans.shape[1] == 1 and trans.shape[-1] == 3
+        # assert angles.shape[1] == trans.shape[1] == 1 and trans.shape[-1] == 3
 
-        # ---- Expeirments below -------
-
-        res_trans = None
-        if self.include_res_trans:
-            squeezed_feature = tf.keras.layers.AveragePooling2D()(squeezed_feature)
-            res_trans = self.conv_res_motion(squeezed_feature)
-
-            lite_mode_idx = [len(input_features)-1]
-            for i in range(len(input_features)-1, -1, -1):  # decoding, last one excluded
-                # t1 = time.perf_counter()
-                use_lite_mode = i in lite_mode_idx
-                # print('idx=%d uses lite_mode? ' % i, use_lite_mode)
-                res_trans = self._refine_motion_field(res_trans, input_features[i],
-                                                      idx=i, lite_mode=use_lite_mode)
-                # print(res_trans.shape, input_features[i].shape, "%.2fms" % ((time.perf_counter() - t1) * 1000))
-
-            res_trans *= self.pose_scale
-            if self.do_automasking:
-                res_trans = self.apply_automask_to_res_trans(res_trans)
-
-        return {"angles": angles, "translations": trans, "res_translations": res_trans}
+        return {"angles": angles, "translations": trans}
 
     def padded_conv(self, conv, inp, padding='CONSTANT'):
-        inp = tf.pad(inp, [[0, 0], [1, 1], [1, 1], [0, 0]], mode='CONSTANT')
+        inp = tf.pad(inp, [[0, 0], [1, 1], [1, 1], [0, 0]], mode=padding)
         return conv(inp)
+
+    def decode_background_motion(self, squeezed_feature, num_scales=3):
+        out = self.relu(squeezed_feature)
+        for i in range(num_scales):
+            if i != 2:
+                out = tf.pad(out, [[0, 0], [1, 1], [1, 1], [0, 0]], mode='CONSTANT')
+            out = self.convs_pose[i](out)
+            if i != 2:
+                out = self.relu(out)
+
+        out = tf.reduce_mean(out, [1, 2], keepdims=True)
+        backgrd_motion = tf.reshape(out, [-1, self.pose_num, 1, 6])
+        return backgrd_motion
+
+
+class ResidualTranslationNet(PoseDecoder_exp):
+    def __init__(self, has_depth, lite_mode_idx=(4,3,2,1), do_automasking=False):
+        super(ResidualTranslationNet, self).__init__(has_depth)
+        conv_prop_1 = {'kernel_size': 1, 'strides': 1, 'use_bias': False, 'padding': 'valid'}
+        self.conv_res_motion = Conv2D(self.out_ch, **conv_prop_1, name='unrefined_res_trans')
+        self.num_ch_dec = [self.out_ch, 16, 32, 64, 128, 256]
+        self.lite_mode_idx = lite_mode_idx
+        self.do_automasking = do_automasking
+        self.conv_refine_motion = []
+        for i, ch in enumerate(self.num_ch_dec):
+            idx = len(self.num_ch_dec) - i - 1     # 5->0
+            pref = 'Refine%d' % idx
+            conv_prop_2 = {'filters': ch, 'kernel_size': 3, 'strides': 1,
+                           'activation': 'relu', 'padding': 'valid'}
+            tmp = []
+            for j in range(3):
+                tmp.append(Conv2D(**conv_prop_2, name=pref + 'Conv%d' % (j + 1)))
+            tmp.append(Conv2D(self.out_ch, [1, 1], strides=1,
+                              activation=None, use_bias=False, name=pref + 'Conv4'))
+            self.conv_refine_motion.append(tmp)
+
+    def call(self, features, training=None, mask=None):
+        """ Decoding residual translation
+        features: [input_image, *encoded_features]
+        res_trans_field: Tensor, [B,H,W,1]
+            Residual translation field
+        """
+        squeezed_feature = tf.keras.layers.AveragePooling2D()(features[-1])
+        res_trans_field = self.conv_res_motion(squeezed_feature)
+
+        for i in range(len(features)-1, -1, -1):
+            # t1 = time.perf_counter()
+            use_lite_mode = i in self.lite_mode_idx
+            # print('idx=%d uses lite_mode? ' % i, use_lite_mode)
+            res_trans_field = self._refine_motion_field(res_trans_field, features[i],
+                                                        idx=i, lite_mode=use_lite_mode)
+            # print(res_trans_field.shape, input_features[i].shape, "%.2fms" % ((time.perf_counter() - t1) * 1000))
+
+        res_trans_field *= self.pose_scale
+        if self.do_automasking:
+            res_trans_field = self.apply_automask_to_res_trans(res_trans_field)
+
+        return res_trans_field
 
     def _refine_motion_field(self, motion_field, conv, idx, lite_mode=False):
         """Refine residual motion map"""
@@ -143,15 +160,15 @@ class PoseDecoder_exp(tf.keras.Model):
             conv_inp = tf.concat([output_1, output_2], axis=-1)
         else:
             # use lite mode to save computation
-            conv_inp = self.padded_conv(self.conv_refine_motion[i][0],conv_inp)
+            conv_inp = self.padded_conv(self.conv_refine_motion[i][0], conv_inp)
 
         output = self.conv_refine_motion[i][3](conv_inp)
-        # print('upsamp_motion_field shape', upsamp_motion_field.shape)
         # print('output shape', conv_inp.shape)
         output = upsamp_motion_field + output
         return output
 
-    def apply_automask_to_res_trans(self, residual_trans):
+    @staticmethod
+    def apply_automask_to_res_trans(residual_trans):
         """Masking out the residual translations by thresholding on their mean values."""
         sq_residual_trans = tf.sqrt(
             tf.reduce_sum(residual_trans ** 2, axis=3, keepdims=True))
@@ -164,19 +181,6 @@ class PoseDecoder_exp(tf.keras.Model):
         residual_trans *= mask_residual_translation
         return residual_trans
 
-    def decode_background_motion(self, squeezed_feature, num_scales=3):
-        out = self.relu(squeezed_feature)
-        for i in range(num_scales):
-            if i != 2:
-                out = tf.pad(out, [[0, 0], [1, 1], [1, 1], [0, 0]], mode='CONSTANT')
-            out = self.convs_pose[i](out)
-            if i != 2:
-                out = self.relu(out)
-
-        out = tf.reduce_mean(out, [1, 2], keepdims=True)
-
-        backgrd_motion = tf.reshape(out, [-1, self.pose_num, 1, 6])
-        return backgrd_motion
 
     # def add_intrinsics_head(self, bottleneck, image_height=192, image_width=640):
     #     """Adds a head the preficts camera intrinsics.
@@ -205,43 +209,39 @@ class PoseDecoder_exp(tf.keras.Model):
     #     intrinsic_mat = tf.concat([intrinsic_mat, last_row], axis=1)
     #     return intrinsic_mat
 
-
-def build_posenet(pose_dec, new_version=True):
-    # num_ch_enc = [64, 64, 128, 256, 512]
+def make_data(new_version=True):
     if new_version:
-        shapes = [(2,192,640,4), (2, 64, 96, 320), (2, 48, 160, 64), (2, 24, 80, 128), (2, 12, 40, 256), (2, 6, 20, 512)]
+        shapes = [(2,192,640,4), (2, 96, 320, 64), (2, 48, 160, 64), (2, 24, 80, 128), (2, 12, 40, 256), (2, 6, 20, 512)]
     else:
-        shapes = [(2, 64, 96, 320), (2, 48, 160, 64), (2, 24, 80, 128), (2, 12, 40, 256),
-                  (2, 6, 20, 512)]
+        shapes = [(2, 96, 320, 64), (2, 48, 160, 64), (2, 24, 80, 128), (2, 12, 40, 256), (2, 6, 20, 512)]
     dummy_inputs = [tf.random.uniform(shape=(shapes[i])) for i in range(len(shapes))]
+    return dummy_inputs
+
+def build_posenet(pose_dec, dummy_inputs):
+    # num_ch_enc = [64, 64, 128, 256, 512]
     # pose_dec = PoseDecoder(num_ch_enc, num_input_features=1, num_frames_to_predict_for=1, stride=1)
     outputs = pose_dec(dummy_inputs)
     return pose_dec, dummy_inputs, outputs
 
 
 def exp_res_trans():
-    pose_dec = PoseDecoder_exp(pose_num=1, has_depth=True)
-    pose_dec, dum_inp, outputs = build_posenet(pose_dec)
-    pose_dec1 = PoseDecoder(num_frames_to_predict_for=1)
-    pose_dec1, dum_inp1, outputs1 = build_posenet(pose_dec1, new_version=False)
+    # pose_dec = PoseDecoder_exp(pose_num=1, has_depth=True)
+    dummy_inp = make_data(new_version=True)
+    # pose_dec, dum_inp, outputs = build_posenet(pose_dec)
+    # pose_dec1 = PoseDecoder(num_frames_to_predict_for=1)
+    # pose_dec1, dum_inp1, outputs1 = build_posenet(pose_dec1, new_version=False)
+    res_trans_net = ResidualTranslationNet(has_depth=True)
+    res_trans_net(dummy_inp)
 
     # pose_dec.summary()
     t0 = time.perf_counter()
     for i in range(100):
         t1 = time.perf_counter()
-        pose_dec(dum_inp)
+        output = res_trans_net(dummy_inp)
         print("{:.3f}".format(time.perf_counter()-t1))
     print("average:", (time.perf_counter() - t0)/100)
 
-    t0 = time.perf_counter()
-    for i in range(100):
-        t1 = time.perf_counter()
-        pose_dec1(dum_inp1)
-        print("{:.3f}".format(time.perf_counter()-t1))
-    print("average:", (time.perf_counter() - t0)/100)
-
-    for k,v in outputs.items():
-        print(k, v.shape)
+    print(output.shape)
 
 
 if __name__ == '__main__':
@@ -250,8 +250,6 @@ if __name__ == '__main__':
     #     print(k,"\t", v.shape)
     # print(a.shape)
     exp_res_trans()
-
-
 
 
 # -------- Archived below ---------
