8441a5f708d859761b49bafbaba1f073e4587187
dexter2406 <46349448+dexter2406@users.noreply.github.com>
2021-05-23 00:33:20 +0200

    add decoder_adpative_size, but not in use

--- models/depth_decoder_creater.py
+++ models/depth_decoder_creater.py
@@ -3,7 +3,6 @@ import cv2 as cv
 from collections import OrderedDict
 from tensorflow.keras.layers import Conv2D
 import numpy as np
-import time
 
 
 class DepthDecoder_full(tf.keras.Model):
@@ -62,6 +61,7 @@ class DepthDecoder_full(tf.keras.Model):
             padding = 'same'
         conv = Conv2D(filters=filter_num, kernel_size=3, activation=activate_type,
                       strides=1, padding=padding, use_bias=True, name=name)
+
         return conv
 
     """
@@ -94,12 +94,98 @@ class DepthDecoder_full(tf.keras.Model):
         return outputs
 
 
+class DepthDecoder_AdaptiveSize(tf.keras.Model):
+    def __init__(self):
+        super(DepthDecoder_AdaptiveSize, self).__init__()
+        self.num_ch_enc = [64, 64, 128, 256, 512]
+        self.num_ch_dec = [16, 32, 64, 128, 256]
+        self.scales = [0,1,2,3]  # range(4)
+        self.num_output_channels = 1
+
+        self.convs_1 = [None]*len(self.num_ch_dec)
+        self.convs_2 = [None]*len(self.num_ch_dec)
+
+        # todo: dispconv can be multiple output
+        self.dispconv = [None]*len(self.scales)
+
+        for i in range(4, -1, -1):
+            # upconv_0
+            num_ch_in = self.num_ch_enc[-1] if i == 4 else self.num_ch_dec[i + 1]
+            num_ch_out = self.num_ch_dec[i]
+            # in: 512, 256, 128, 64, 32
+            # out: 256, 128, 64, 32, 16
+            self.convs_1[i] = self.make_conv(num_ch_in, num_ch_out, pad_mode='reflect', activate_type='elu',
+                                             type='conv_0', index=i)
+
+            # upconv_1
+            num_ch_in = self.num_ch_dec[i]
+            if i > 0:
+                num_ch_in += self.num_ch_enc[i - 1]
+            num_ch_out = self.num_ch_dec[i]
+            # in: 256+256, 128+128, 64+64, 32+64, 16
+            # out: 256, 128, 64, 32, 16
+            # self.convs[("upconv", i, 1)] = TF_ConvBlock(num_ch_in, num_ch_out, pad_mode='reflect')
+            self.convs_2[i] = self.make_conv(num_ch_in, num_ch_out, pad_mode='reflect', activate_type='elu',
+                                             type='conv_1', index=i)
+
+        for s in self.scales:
+            # self.convs[("dispconv", s)] = TF_Conv3x3(self.num_ch_dec[s], self.num_output_channels, pad_mode='reflect')
+            num_ch_in = self.num_ch_dec[s]
+            num_ch_out = self.num_output_channels
+            self.dispconv[s] = self.make_conv(num_ch_in, num_ch_out, activate_type=None,
+                                              pad_mode='reflect', type='disp', index=s)
+        # self.dispconv_0 = self.make_conv(self.num_ch_dec[0], self.num_output_channels, activate_type=None,
+        #                                  pad_mode='reflect', type='disp', index=0)
+
+    def make_conv(self, input_channel, filter_num, activate_type=None, pad_mode='reflect',
+                  type:str=None, index=-1, input_shape:tuple=None):
+        name = None
+        if type is not None and index != -1:
+            name = ''.join([type, '_%d'%index])
+        if pad_mode == 'reflect':
+            padding = 'valid'
+        else:
+            padding = 'same'
+        conv = Conv2D(filters=filter_num, kernel_size=3, activation=activate_type,
+                      strides=1, padding=padding, use_bias=True, name=name)
+        return conv
+
+    def call(self, input_features, training=None, mask=None):
+        ch_axis = 3
+        x = input_features[-1]
+        outputs = {}
+        for i in range(4, -1, -1):
+            x = tf.pad(x, [[0, 0], [1, 1], [1, 1], [0, 0]], mode='REFLECT')
+            # x = self.convs[("upconv", i, 0)](x)
+            x = self.convs_1[i](x)
+            x = [tf.keras.layers.UpSampling2D()(x)]
+            if i > 0:
+                skip = input_features[i - 1]
+                skip = tf.image.resize(skip, x[0].shape[1:3])
+                x += [skip]
+            x = tf.concat(x, ch_axis)
+            x = tf.pad(x, [[0, 0], [1, 1], [1, 1], [0, 0]], mode='REFLECT')
+            # x = self.convs[("upconv", i, 1)](x)
+            x = self.convs_2[i](x)
+            if i in self.scales:
+                out = tf.pad(x, [[0, 0], [1, 1], [1, 1], [0, 0]], mode='REFLECT')
+                out = self.dispconv[i](out)
+                outputs["output_%d" % i] = tf.math.sigmoid(out)
+
+        # x = tf.pad(x, [[0, 0], [1, 1], [1, 1], [0, 0]], mode='REFLECT')
+        # x = self.convs[("dispconv", 0)](x)
+        # x = self.dispconv[0](x)
+        # disp0 = tf.math.sigmoid(x)
+
+        return outputs
+
+
 class DepthDecoder_one_output(tf.keras.Model):
     def __init__(self):
         super(DepthDecoder_one_output, self).__init__()
         self.num_ch_enc = [64, 64, 128, 256, 512]
         self.num_ch_dec = [16, 32, 64, 128, 256]
-        self.scales = [0, 1, 2, 3]  # range(4)
+        self.scales = [0,1,2,3]  # range(4)
         self.num_output_channels = 1
 
         self.convs_0 = [None]*len(self.num_ch_dec)
@@ -175,9 +261,11 @@ class DepthDecoder_one_output(tf.keras.Model):
                 x = tf.pad(x, [[0, 0], [1, 1], [1, 1], [0, 0]], mode='REFLECT')
                 x = self.dispconv[i](x)
                 outputs.append(tf.math.sigmoid(x))
+        # x = tf.pad(x, [[0, 0], [1, 1], [1, 1], [0, 0]], mode='REFLECT')
+        # x = self.dispconv_0(x)
+        # disp0 = tf.math.sigmoid(x)
 
         return outputs
-
 
 def decoder_load_weights(decoder,
                          weights_path=None):
@@ -206,7 +294,7 @@ def decoder_load_weights(decoder,
 
 
 def build_model(inputs, model_type=None, verbose=True, check_output=False, save=False):
-    decoder = DepthDecoder()
+    decoder = DepthDecoder_full()
     outputs = decoder.predict(inputs)
     # decoder = decoder_load_weights(decoder)
     if verbose:
@@ -295,13 +383,12 @@ if __name__ == '__main__':
               tf.random.uniform(shape=(1,24, 80, 128)),
               tf.random.uniform(shape=(1,12, 40, 256)),
               tf.random.uniform(shape=(1,6, 20, 512))]
-    decoder = DepthDecoder_full()
-    # decoder = build_model(inputs, model_type='keras', verbose=True)
-    for i in range(100):
-        t1 = time.perf_counter()
-        outputs = decoder(inputs)
-        print(time.perf_counter()-t1)
-        print('==========================')
+
+    decoder = build_model(inputs, model_type='keras', verbose=True)
+    decoder.summary()
+    for i in range(6):
+        decoder = frozen_depth_dec(dep_dec=decoder, num_frozen=i)
+    a = 0
     # weights_path_disp = r"D:\MA\Recources\monodepth2-torch\models\depthdecoder_weights_full_outs.pkl"
     # saved_model_path = r"D:\MA\Recources\monodepth2-torch\models\depth_decoder_singlet"
     # loaded_layer_record = []
@@ -309,5 +396,4 @@ if __name__ == '__main__':
     # decoder = add_otherconvs_weights(decoder, saved_model_path, loaded_layer_record)
     # print("loaded layers are: ")
     # print(loaded_layer_record)
-    #
     # tf.keras.models.save_model(decoder, 'models/decoder_full-test')
